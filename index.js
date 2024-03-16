6eb44e637235f34ffd1c6f74a93df8c596e6a830
stephy <stephshiro35@gmail.com>
2024-03-16 16:56:31 +0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solutions in this file
+// Code your solutions in this 
+const names = ["Guadalupe", "Ollie", "Aki"];
+const thankYouCards = writeCards(names, event);
+console.log(thankYouCards);
+
+
+
+function writeCards(namesArray, eventName) {
+
+    const cards = [];
+    for (let i = 0; i < namesArray.length; i++) {
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+
+function countDown(number) {
+    if (number < 0) {
+        console.log("10");
+        return;
+    }
+
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
